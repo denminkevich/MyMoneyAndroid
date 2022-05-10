bacc6cb523134e2e4081c12db7ffa8d619dceefd
denis.minkevich <minkevich2001@gmail.com>
2022-05-10 12:07:36 +0300

    Completed design of first screen

--- app/src/main/java/com/example/mymoney/MainActivity.java
+++ app/src/main/java/com/example/mymoney/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mymoney;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/mymoney/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mymoney/ui/home/HomeFragment.java
@@ -14,20 +14,33 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mymoney.R;
 
+import java.util.Calendar;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        String[]monthName={"Январь","Февраль","Март", "Апрель", "Май", "Июнь", "Июль",
+                "Август", "Сентябрь", "Октябрь", "Ноябрь",
+                "Декабрь"};
+        String month = monthName[c.get(Calendar.MONTH)];
+        int year = c.get(Calendar.YEAR);
+
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        TextView monthText = root.findViewById(R.id.month);
+        TextView yearText = root.findViewById(R.id.year);
+        monthText.setText(month);
+        yearText.setText(Integer.toString(year));
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
